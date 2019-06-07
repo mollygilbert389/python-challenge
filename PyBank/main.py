7a1bf2933ea9a3ab07519bb761d68d271c7d1011
Molly Gilbert <msgilbert389@gmail.com>
2019-06-07 15:57:07 -0500

    adding final average function

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,7 @@ csvpath = "Resources/pyBankData.csv"
 months = 0
 monthsArray = {}
 netProfitOrLosses = 0
-netProfitOrLossesArray = [0]
+netProfitOrLossesArray = []
 averageProfitOrLosses = 0
 averageChangeProfitOrLosses = 0
 greatestIncrease = 0
@@ -16,14 +16,9 @@ with open (csvpath, newline="") as csvfile:
 
 ## Month counter
     for row in csvreader: 
+        netProfitOrLossesArray.append(row[1])
         monthsArray[months] = row
         months = months+1
-
-## Net Profit/Losses Counter
-    for row in csvreader: 
-        netProfitOrLossesArray.append(int(row[1]))
-
-
 
 print("Finanical Analysis")
 print("------------------------------------------------")
@@ -31,15 +26,22 @@ print("------------------------------------------------")
 # ## prints months
 row = len(monthsArray[0])
 column = len(monthsArray) -1
-print("Total Months: " +  str(column))
+print("Total Months:",  column)
 
 ## prints total gains/losses
-netProfitOrLosses = sum(map(int, netProfitOrLossesArray))
-print(netProfitOrLosses)
-# print("Total: " + str(total))
-
+netProfitOrLossesArray.remove('Profit/Losses')
+netProfitOrLossesArray = list(map(int,netProfitOrLossesArray))
+netProfitOrLosses = sum(netProfitOrLossesArray)
+print("Total Profits/Losses:", '${:,.2f}'.format(netProfitOrLosses))
 
 ## prints Average Change
+averageProfitOrLosses = column / netProfitOrLosses
+## 1st(A) number to second(B) number for 43 number pairs then take their number and add them all up
+## calculate difference between number A and B = c
+## push that number into an array 
+## caluclate the sum of that array
+## divide the increase by the original number and multiply by 100*
+print("Average Change:", averageProfitOrLosses)
 
 ## Prints Greatest Increase
 
