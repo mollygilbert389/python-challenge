b376c6bc2f612a1fa13eb5073f361edc574c567f
Molly Gilbert <msgilbert389@gmail.com>
2019-06-17 14:24:58 -0500

    Adding final pyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,46 +4,49 @@ csvpath = "Resources/pyBankData.csv"
 file_to_output = "analysis/budget_analysis_1.txt"
 
 months = 0 
-monthsArray = {}
 netProfitOrLosses = 0
-netProfitOrLossesArray = []
 monthOfChange = []
 previousRevenue = 0
 revChangeList = []
+revChange = 0
+revAverage = 0
 greatestIncrease = ["", 0]
 greatestDecrease = ["", 9999999999999999999]
 totalRevenue = 0
 
-with open (csvpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open (csvpath) as revenuData:
+    reader = csv.DictReader(revenuData)
+ 
+    for row in reader: 
+        months = months + 1
+        netProfitOrLosses = netProfitOrLosses + int(row["Profit/Losses"])
 
-## Month counter
-    for row in csvreader: 
-        netProfitOrLossesArray.append(row[1])
-        monthsArray[months] = row
-        months = months+1 
-        revenueChange = int(row[1]) - previousRevenue
-        previousRevenue - int(row[1])
-        revChangeList = revChangeList + [revenueChange]
-        monthOfChange = monthOfChange - [row[1]]
+        revChange = int(row["Profit/Losses"]) - previousRevenue
+        previousRevenue = int(row["Profit/Losses"])
+        revChangeList = revChangeList + [revChange]
+        monthOfChange = monthOfChange + [row["Date"]]
 
-print("Finanical Analysis")
-print("------------------------------------------------")
+        if (revChange > greatestIncrease[1]): 
+            greatestIncrease[0] = row["Date"]
+            greatestIncrease[1] = revChange
 
-# ## prints months
-row = len(monthsArray[0])
-column = len(monthsArray) -1
-print("Total Months:",  column)
+        if (revChange < greatestDecrease[1]): 
+            greatestDecrease[0] = row["Date"]
+            greatestDecrease[1] = revChange
 
-## prints total gains/losses
-netProfitOrLossesArray.remove('Profit/Losses')
-netProfitOrLossesArray = list(map(int,netProfitOrLossesArray))
-netProfitOrLosses = sum(netProfitOrLossesArray)
-print("Total Profits/Losses:", '${:,.2f}'.format(netProfitOrLosses))
+revAverage = sum(revChangeList) / len(revChangeList)
 
-## prints Average Change
-print("Average Change:" revenueChange)
+output = (
+    f"\nFinanical Analysis\n"
+    f"------------------------------------------------\n"
+    f"Total Months:  {months}\n"
+    f"Total Revenue: ${netProfitOrLosses}\n"
+    f"Average Revenue Change: ${revAverage}\n"
+    f"Greatest Increase in Revenue: {greatestIncrease[0]} (${greatestIncrease[1]})\n"
+    f"Greatest Decrease in Revenue: {greatestDecrease[0]} (${greatestDecrease[1]})\n"
+)
 
-## Prints Greatest Increase
+print(output)
 
-## Prints Greatest Decrease
+with open(file_to_output, "w") as txt_file: 
+    txt_file.write(output)
